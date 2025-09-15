a0ff7ab3f608c286d324d2eacdefee06d4942ca3
HTorike <hacchimantorike@gmail.com>
2025-09-15 15:23:01 -0300

    addition of the first section

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 <script setup lang="ts">
   import Header from './components/Header.vue';
   import Hero from './components/Hero.vue';
+  import WorkManagement from './components/WorkManagement.vue'
 </script>
 
 <template>
   <Header />
   <Hero />
+  <WorkManagement />
 </template>
---
+++ src/components/WorkManagement.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+    import imgContainer2 from '../assets/img/Image-container2.svg'
+
+</script> 
+
+<template>
+
+    <section class="secWorkManagement">
+
+        <div>
+            <div>
+                <h1>Project Management</h1>
+                <p>
+                    Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.
+                </p>
+            </div>
+            <button>
+                Get Started
+            </button>
+        </div>
+        <div>
+            <img :src="imgContainer2" alt="">
+        </div>
+
+    </section>
+
+</template>
+
+<style scoped>
+
+    .secWorkManagement {
+        display: flex;
+        padding: 8.7em 13.7em;
+    }
+
+</style>
